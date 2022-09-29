1558797f1c5a7a293a8702b2b35a9dc159701aa9
vitechjayant <jayantsoni@gmail.con>
2022-09-29 14:48:02 +0530

    test build

--- src/main/java/com/democompany/first_demo_project/App.java
+++ src/main/java/com/democompany/first_demo_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! new build" );
+        System.out.println( "Hello World! new build 2" );
     }
 }
